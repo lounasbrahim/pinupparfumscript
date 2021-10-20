c5d7edf2fe40e457deeee4bca561f06022e0f175
loun <loun@pop-os.localdomain>
2021-10-20 16:29:06 +0100

    first commit

--- main.js
+++ main.js
@@ -1,11 +1,37 @@
-var cron = require("node-cron");
-var shell = require("shelljs");
 var request = require("request");
+const winston = require("winston");
 
-request({
-        uri: "http://pinupparfum.fr/modules/ps_reminder/cron.php?secure_key=UDAPIESARKKIX2RT",
-    },
-    function(error, response, body) {
-        console.log(response);
-    }
+var myFormat = winston.format.combine(
+  winston.format.timestamp({
+    format: "YYYY-MM-DD HH:mm:ss",
+  }),
+  winston.format.json()
+);
+
+const logger = winston.createLogger({
+  level: "info",
+  format: myFormat,
+  transports: [
+    new winston.transports.File({ filename: "error.log", level: "error" }),
+    new winston.transports.File({ filename: "infos.log" }),
+  ],
+});
+
+if (process.env.NODE_ENV !== "production") {
+  logger.add(
+    new winston.transports.Console({
+      format: winston.format.simple(),
+    })
+  );
+}
+
+request(
+  {
+    uri: "http://pinupparfum.fr/modules/ps_reminder/cron.php?secure_key=UDAPIESARKKIX2RT",
+  },
+  function (error, response, body) {
+    logger.info("url: ", response.request.uri.href);
+    logger.info("statusCode: ", response.statusCode);
+    logger.info("statusMessage", response.statusMessage);
+  }
 );
